d3a657cdb1ae25cfd9e76bf514251af1768474c8
Martin Amechazurra <mamechaz@ucm.es>
2020-04-26 19:22:32 +0200

    Added lamps
    - Lámparas sobre el patio de butacas
    - Comportamiento de subida y bajada, ajustable desde editor
    - Palancas como triggers en los palcos, por el momento se activan pulsando "e" con el jugador a 3.5 de distancia o menos. Queda ajustar que roten suavemente al accionarlas

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+
+    public UnityEngine.Vector3 UpPosition;
+    public float UpVelocity;
+    public UnityEngine.Vector3 DownPosition;
+    public float DownVelocity;
+
+    /// <summary>
+    /// True if PULLED (UP), false if DROPPED (Down)
+    /// </summary>
+    private bool lampState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lampState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lampState)
+        {
+            if(transform.position.y < UpPosition.y)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, UpPosition, Time.deltaTime * UpVelocity);
+            }
+        }
+        else
+        {
+            if (transform.position.y > DownPosition.y)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, DownPosition, Time.deltaTime * DownVelocity);
+            }
+        }
+        
+    }
+
+    public bool isUp()
+    {
+        return lampState;
+    }
+
+    public void toggleLampState()
+    {
+        lampState = !lampState;
+    }
+
+}
---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Lamp lampara;
+    public float leverSpeed;
+
+    private bool leverState;
+    private Quaternion truePosition; // Posición cuando leverState es true
+    private Quaternion falsePosition; // Posición cuando leverState es false
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leverState = lampara.isUp();
+        transform.parent.transform.Rotate(0.0f, 0.0f, -45.0f, Space.World);
+
+        truePosition = Quaternion.Euler(0.0f, 0.0f, -45.0f);
+        falsePosition = Quaternion.Euler(0.0f, 0.0f, 45.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leverState)
+        {
+            if(transform.parent.transform.rotation.eulerAngles.z > -45.0f)
+            {
+                transform.parent.transform.rotation = Quaternion.Slerp(transform.rotation, truePosition, Time.deltaTime * leverSpeed);
+            }
+        }
+        else
+        {
+            if (transform.parent.transform.rotation.eulerAngles.z < 45.0f)
+            {
+                transform.parent.transform.rotation = Quaternion.Slerp(transform.rotation, falsePosition, Time.deltaTime * leverSpeed);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            leverState = !leverState;
+            lampara.toggleLampState();
+        }
+    }
+
+}
