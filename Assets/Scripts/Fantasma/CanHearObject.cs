0d40a529f9c3d4e1635ab46e2e510f89929918ea
Martin Amechazurra <mamechaz@ucm.es>
2020-04-29 15:28:29 +0200

    Implemented "State_SalaMusica" Behaviour

---
+++ Assets/Scripts/Fantasma/CanHearObject.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks.Tutorials
+{
+    [TaskCategory("Tutorial")]
+    [TaskIcon("Assets/Behavior Designer Tutorials/Tasks/Editor/{SkinColor}CanSeeObjectIcon.png")]
+    public class CanHearObject : Conditional
+    {
+        [Tooltip("The object that we are searching for")]
+        public GameObject targetObject;
+
+        [Tooltip("The tag of the GameObject to check for a trigger against")]
+        public string tag = "";
+        [Tooltip("The object that entered the trigger")]
+        public SharedGameObject otherGameObject;
+
+
+        private bool triggered = false;
+        private bool target = false;
+
+        /// <summary>
+        /// Returns success if an object was found otherwise failure
+        /// </summary>
+        /// <returns></returns>
+        public override TaskStatus OnUpdate()
+        {
+
+            if (target)
+            {
+                // Return success if target was found
+                return TaskStatus.Success;
+            }
+            // Target object is not within hearing so return failure
+            return TaskStatus.Failure;
+        }
+
+        public override void OnEnd()
+        {
+            triggered = false;
+            target = false;
+        }
+
+        public override void OnReset()
+        {
+            triggered = false;
+            target = false;
+        }
+
+        private bool checkSoundSource (GameObject other)
+        {
+            if(other.gameObject.Equals(targetObject))
+            {
+                Debug.Log("SUCCESS");
+                return true;
+            }
+            else return false;
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            Debug.Log(other.gameObject.tag);
+            if (other.gameObject.tag == tag)
+            {
+                otherGameObject.Value = other.gameObject;
+                triggered = true;
+                target = checkSoundSource(other.gameObject);
+            }
+        }
+
+        public override void OnTriggerEnter(Collider other)
+        {
+            
+            if (other.gameObject.tag == tag)
+            {
+                Debug.Log("TRIGGERED");
+                otherGameObject.Value = other.gameObject;
+                triggered = true;
+                target = checkSoundSource(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Fantasma/FixMusicRoom.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    [TaskDescription("Returns success when MusicRoom is fixed")]
+    [TaskCategory("Phantom")]
+    public class FixMusicRoom : Conditional
+    {
+
+        private bool fixedRoom = false;
+        public override TaskStatus OnUpdate()
+        {
+            return fixedRoom ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+        public override void OnEnd()
+        {
+            fixedRoom = false;
+        }
+
+        public override void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Musica") && other is SphereCollider)
+            {
+                other.gameObject.GetComponent<Instruments>().fixInstruments();
+                fixedRoom = true;
+            }
+        }
+
+        public override void OnReset()
+        {
+            fixedRoom = false;
+        }
+    }
+}
